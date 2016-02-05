8e207828acde8844baeeda77d2c5b5b1dcb57c17
Haris1337 <Owxharis@hotmail.com>
2016-02-05 13:14:00 +0100

    grr
    hn

--- hei.py
+++ hei.py
@@ -1,1 +1,8 @@
-Haris er kul
+# -*- coding: utf-8 -*-
+from sys import argv
+
+def func1(name1, name2):
+    return "Hei, " + name1 + "og " + name2
+
+name1, name2 = argv[1], argv[2]
+print func1(name1,name2)
